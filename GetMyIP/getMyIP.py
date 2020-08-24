17dfd3cb083c7d33bcc35bdf495318a6981c82ff
Ehsan Mokhtari <ehsan76m@gmail.com>
2020-08-25 00:36:50 +0430

    Delete getMyIP.py

--- GetMyIP/getMyIP.py
+++
@@ -1,9 +0,0 @@
-import socket
-while True:
-    host_name = socket.gethostname()
-    ip_address = socket.gethostbyname(host_name)
-    print("your IP address is : ",ip_address)
-    print("if you are using vpn, for reloading your new ip address press Enter")
-    print("*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#")
-    if(input()):
-        pass
